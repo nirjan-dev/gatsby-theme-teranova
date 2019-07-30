de86c899e6f08b5fbe811a8a06956ada4e5f4566
Nirjan Khadka <nk13@pop-os.localdomain>
2019-07-30 12:15:30 +0545

    add theme-ui and material react

---
+++ gatsby-theme-teranova/src/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  fonts: {
+    body: '"Roboto",sans-serif',
+    heading: '"Volkron",sans-serif',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    heading: '700',
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.7,
+    heading: 1.2
+  },
+  letterSpacings: {
+    body: 'normal',
+    caps: '0.2rem'
+  },
+  colors: {
+    text: '#333',
+    primary: 'rebeccapurple',
+    textAlt: '#fcfcfc',
+    background: '#f9f9f9',
+    secondary: 'tomato',
+    accent: 'lime',
+    muted: 'e2e2e2',
+  },
+};
